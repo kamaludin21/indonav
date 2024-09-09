5ad3d4b6b62c965fec68edaa2c021617edb6c79e
kamal <kamal.zyel@gmail.com>
2024-09-09 07:48:35 +0700

    fix build

--- resources/views/pages/support/inquiry.blade.php
+++ resources/views/pages/support/inquiry.blade.php
@@ -5,7 +5,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="max-w-screen-lg space-y-10 py-20 mx-auto px-2 md:px-0 px-2 md:px-0">
+  <div class="max-w-screen-lg space-y-10 py-20 mx-auto px-2 md:px-0">
     <div class="text-start space-y-4 border-b pb-2">
       <h2 class="text-2xl md:text-4xl font-bold text-orange-400">Send an Inquiry</h2>
       <p class="text-base md:text-xl text-slate-500">Discover what we can do for you</p>
