69631d6ef9fd4a5a65b4b07b03acb33194d1f0d0
kamal <kamal.zyel@gmail.com>
2024-09-21 16:26:32 +0700

    profile plugins

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Joaopaulolndev\FilamentEditProfile\FilamentEditProfilePlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -55,6 +56,12 @@ class AdminPanelProvider extends PanelProvider
       ])
       ->authMiddleware([
         Authenticate::class,
+      ])
+      ->plugins([
+        FilamentEditProfilePlugin::make()
+          ->setTitle('My Profile')
+          ->setNavigationLabel('My Profile')
+          ->setIcon('heroicon-o-user')
       ]);
   }
 }
