a47c7baf75da69ce106deca6a01bb5c2183b4688
Kamaludin <kamal.zyel@gmail.com>
2025-06-21 14:03:45 +0700

    symlink

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
-Artisan::call('storage:link');
 use App\Http\Controllers\IndustryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/industries/{slug}', [IndustryController::class, 'view'])->name('industry.view');
+
+Route::get('foo', function(){
+    $targetFolder = $_SERVER['DOCUMENT_ROOT'].'/storage/app/public';
+    $linkFolder = $_SERVER['DOCUMENT_ROOT'].'/storage';
+    symlink($targetFolder, $linkFolder);
+    return 'success';
+});
