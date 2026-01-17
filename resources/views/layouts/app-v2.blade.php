18c3e252d32fa26a35141585b3a3bfac830d048a
Kamaludin <kamal.zyel@gmail.com>
2026-01-17 11:57:28 +0700

    security dependecies update
    - remove axios
    - update livewire 3.7
    - update vite 7

--- resources/views/layouts/app-v2.blade.php
+++ resources/views/layouts/app-v2.blade.php
@@ -230,7 +230,7 @@
         {{-- Socmed --}}
         <div class="flex gap-2 items-center">
           {{-- Linkedin Icon --}}
-          <a href="{!! $sites['linkedin']?->url !!}" target="_blank">
+          {{-- <a href="{!! $sites['linkedin']?->url !!}" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-auto" viewBox="0 0 24 24" fill="none"
               stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"
               class="icon icon-tabler icons-tabler-outline icon-tabler-brand-linkedin">
@@ -241,7 +241,7 @@
               <path d="M16 16v-3a2 2 0 1 0 -4 0" />
               <path d="M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z" />
             </svg>
-          </a>
+          </a> --}}
 
           <a href="{!! $sites['instagram']?->url !!}" target="_blank">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-auto" viewBox="0 0 24 24" fill="none"
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
 
 Route::post('/temp/upload', [TicketController::class, 'upload'])->name('temp.upload');
 Route::delete('/temp/revert', [TicketController::class, 'revert'])->name('temp.revert');
+
+Route::get('/check-version-db', function () {
+  $db_cechk = DB::select('select version() as version');
+  return $db_cechk;
+});
